6ba1dbe48f4c710d4d6f8bf43f65e59cb29ab49d
Subrata Mondal <subratamondal1029@outlook.com>
2024-10-06 10:53:38 +0530

    mongodb conection

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-import express from "express";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-
-app.get("/", (req, res)=>{
-    res.send("Hello World");
+dotenv.config({
+    path: './env'
 })
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-})
+
+connectDB();
