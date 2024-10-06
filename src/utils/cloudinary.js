709dbbb575918f5b46eb079cf5d45aaa326e0e06
Subrata Mondal <subratamondal1029@outlook.com>
2024-10-06 19:21:37 +0530

    configure file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Upload a file
+const uploadFile = async (filePath) => {
+    try {
+        if(!filePath) return null;
+
+        // upload in cloudinary
+      const fileResponse = await cloudinary.uploader.upload(filePath, {resource_type: "auto"})
+
+        // if file uploaded
+        console.log("File Uploaded", fileResponse);
+        return fileResponse;
+    } catch (error) {
+        fs.unlinkSync(filePath)
+        return null;
+    }
+}
+
+export { uploadFile }
