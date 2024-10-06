ff67d8a107cb84670f21be67812a153de0564b1d
Subrata Mondal <subratamondal1029@outlook.com>
2024-10-06 11:42:43 +0530

    utils code for error and response

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res
+//       .status(error.code || 500)
+//       .json({ success: false, message: error.message || "Internal Server Error" });
+//   }
+// };
+
+const asyncHandler = (fn) => (
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch((error) => {
+      res
+        .status(error.code || 500)
+        .json({
+          success: false,
+          message: error.message || "Internal Server Error",
+        });
+    });
+  }
+);
+
+export default asyncHandler;
