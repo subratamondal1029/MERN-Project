1ca1eb16eef2859f2d1fd3d2e94f4f233dd3cfe2
Subrata Mondal <subratamondal1029@outlook.com>
2024-10-23 09:31:46 +0530

    add all update routes in router

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,29 +1,47 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser, refreshAccessToken, getCurrentUser } from "../controllers/users.controllers.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changePassword,
+  getCurrentUser,
+  updateUserDetails,
+  updatedUserAvatar,
+  updatedUserCoverImage,
+} from "../controllers/users.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
 );
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 // secured routes
-router.route("/logout").post(verifyJwt, logoutUser)
-router.route("/refreshAccessToken").post(refreshAccessToken)
-router.route("/currentUser").get(verifyJwt, getCurrentUser)
+router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/refreshAccessToken").post(refreshAccessToken);
+router.route("/currentUser").get(verifyJwt, getCurrentUser);
+router.route("/changePassword").post(verifyJwt, changePassword);
+router.route("/updateUserDetails").post(verifyJwt, updateUserDetails);
+router
+  .route("/updatedUserAvatar")
+  .post(verifyJwt, upload.single("avatar"), updatedUserAvatar);
+router
+  .route("/updatedUserCoverImage")
+  .post(verifyJwt, upload.single("coverImage"), updatedUserCoverImage);
 
 export default router;
